76a7e6b4ab74fb92a3fe118f064313e415215f32
Chris <chtz@me.com>
2016-03-06 23:38:13 +0100

    security added

--- doc/samples.sh
+++ doc/samples.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-curl -i -X POST -H "Content-Type:application/json" -d '{ "metric":"foo","value":12.5}' http://localhost:8080/api/measurements
-curl http://localhost:8080/api/measurements/search/findByMetric?metric=foo
+curl --user "user:verySecret!" -i -X POST -H "Content-Type:application/json" -d '{ "metric":"foo","value":12.5}' http://localhost:8080/api/measurements
+curl --user "user:verySecret!" http://localhost:8080/api/measurements/search/findByMetric?metric=foo
